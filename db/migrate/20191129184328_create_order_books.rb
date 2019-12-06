1998a91f5835bb2dd6aba07a850340d70f464330
rosedive <sakindiuweramarie@gmail.com>
2019-12-06 06:11:31 -0800

    migration update

--- db/migrate/20191129184328_create_order_books.rb
+++ db/migrate/20191129184328_create_order_books.rb
@@ -1,4 +1,4 @@
-class CreateOrderBooks < ActiveRecord::Migration[6.0]
+class CreateOrderBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :order_books do |t|
       t.references :user, null: false, foreign_key: true
--- db/migrate/20191129211841_add_approved_to_order_books.rb
+++ db/migrate/20191129211841_add_approved_to_order_books.rb
@@ -1,4 +1,4 @@
-class AddApprovedToOrderBooks < ActiveRecord::Migration[6.0]
+class AddApprovedToOrderBooks < ActiveRecord::Migration[5.2]
   def change
     add_column :order_books, :approved, :boolean
   end
