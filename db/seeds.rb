fd7d7994a36259a7d919f2a9e2fa36d939d433db
rosedive <sakindiuweramarie@gmail.com>
2019-12-06 07:46:43 -0800

    Encrypt User's password in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name:"papa", email: "ga@gmail.com", admin: "true", password:  "1234567890", password_confirmation:"1234567890")
+User.create!(name:"papa", email: "ga@gmail.com", admin: "true", password:  "$2a$10$tbvQuwJKeo7HtsdgiQl36.jPHgyP3BJEUhOkKzY77zxkxTNGh82OC ", password_confirmation:"$2a$10$tbvQuwJKeo7HtsdgiQl36.jPHgyP3BJEUhOkKzY77zxkxTNGh82OC ")
 
     def create_user(email)
     User.create!(
